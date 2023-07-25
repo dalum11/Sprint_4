6e6fcbfd686432195be3c870705217cda2b0bfb1
dalum11 <dalumesandelur@mail.ru>
2023-07-25 20:52:51 +0800

    add project in branch develop

--- src/test/java/pageobject/AboutOrderPage.java
+++ src/test/java/pageobject/AboutOrderPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AboutOrderPage {
 
-    //Драйвер браузера
+    //Драйвер браузерa
     private WebDriver driver;
 
     //Дата, когда привезут самокат
